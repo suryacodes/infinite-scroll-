497cb4afe2d4ee08c1e7372159744f6549ceaec2
suryacodes <62347567+suryacodes@users.noreply.github.com>
2023-07-12 11:05:08 +0530

    1 file modified

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,59 @@ import * as React from 'react';
 import './style.css';
 
 export default function App() {
+  const [items, setItems] = React.useState<any[]>([]);
+  const [isLoading, setIsLoading] = React.useState(false);
+  let [page, setPage] = React.useState(1);
+  const observerTarget = React.useRef(null);
+
+  const fetchData = () => {
+    setIsLoading(true);
+    fetch(`https://jsonplaceholder.typicode.com/todos?_page=${page}&_limit=10`)
+      .then(async (response) => {
+        const data = await response.json();
+        setItems((prev) => [...prev, ...data]);
+        setPage((prevstate) => prevstate + 1);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setIsLoading(false);
+      });
+  };
+
+  React.useEffect(() => {
+    // The Intersection Observer API is used to detect when an element    enters or exits the viewport
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting && !isLoading) {
+          fetchData();
+        }
+      },
+      { threshold: 1 } // element must be visible 100 percent
+    );
+
+    if (observerTarget.current) {
+      observer.observe(observerTarget.current);
+    }
+
+    return () => {
+      if (observerTarget.current) {
+        observer.unobserve(observerTarget.current);
+      }
+    };
+  }, [isLoading]);
+
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <ul>
+        {items.map((item, index) => (
+          <li style={{ marginTop: 100 }} key={index}>
+            {index} {item.title}
+          </li>
+        ))}
+        {items && <li ref={observerTarget}></li>}
+      </ul>
+      {isLoading && <p>Loading...</p>}
     </div>
   );
 }
